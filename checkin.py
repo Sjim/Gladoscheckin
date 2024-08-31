ae768e47bf75d5f2015097f2b856d046f60dfd3c
Sjim <1225747052@qq.com>
2024-09-01 01:05:38 +0800

    print status

--- checkin.py
+++ checkin.py
@@ -42,7 +42,7 @@ if __name__ == '__main__':
             result = checkin.json()     
             # 获取签到结果
             status = result.get('message')
-
+            print(status)
             # 获取账号当前状态
             result = state.json()
             print(result)
